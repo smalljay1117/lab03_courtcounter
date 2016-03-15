cdb5f35e0677d7353ceafbce00e1b879547b463a
smalljay1117 <smalljay1117>
2016-03-15 11:51:34 +0800

    初始

--- app/src/main/java/com/example/android/lab03_courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/lab03_courtcounter/MainActivity.java
@@ -2,12 +2,40 @@ package com.example.android.lab03_courtcounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private int scoreTeamA;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private void displayForTeamA(int score) {
+        TextView scoreView = (TextView)findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    public void add3ForTeamA(View view) {
+        scoreTeamA += 3;
+        displayForTeamA(scoreTeamA);
+    }
+
+    public void add2ForTeamA(View view) {
+        scoreTeamA += 2;
+        displayForTeamA(scoreTeamA);
+    }
+
+    public void add1ForTeamA(View view) {
+        scoreTeamA += 1;
+        displayForTeamA(scoreTeamA);
+    }
+
+    public void resetScoreView(View view) {
+        scoreTeamA = 0;
+        displayForTeamA(scoreTeamA);
     }
 }
