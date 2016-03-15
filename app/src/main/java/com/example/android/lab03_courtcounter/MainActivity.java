b1c6cde957da24867ea7e8679df1e01e574ce0a1
smalljay1117 <smalljay1117>
2016-03-15 12:12:58 +0800

    TeamA vs TeamB

--- app/src/main/java/com/example/android/lab03_courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/lab03_courtcounter/MainActivity.java
@@ -7,6 +7,7 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private int scoreTeamA;
+    private int scoreTeamB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,9 +15,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    private void displayForTeamA(int score) {
-        TextView scoreView = (TextView)findViewById(R.id.team_a_score);
-        scoreView.setText(String.valueOf(score));
+    private void displayForTeamA(int scoreA) {
+        TextView scoreView = (TextView) findViewById(R.id.teamA_a_score);
+        scoreView.setText(String.valueOf(scoreA));
     }
 
     public void add3ForTeamA(View view) {
@@ -34,8 +35,35 @@ public class MainActivity extends AppCompatActivity {
         displayForTeamA(scoreTeamA);
     }
 
+    private void displayForTeamB(int scoreB) {
+        TextView scoreView = (TextView) findViewById(R.id.teamB_a_score);
+        scoreView.setText(String.valueOf(scoreB));
+    }
+
+    public void add3ForTeamB(View view) {
+        scoreTeamB += 3;
+        displayForTeamB(scoreTeamB);
+    }
+
+    public void add2ForTeamB(View view) {
+        scoreTeamB += 2;
+        displayForTeamB(scoreTeamB);
+    }
+
+    public void add1ForTeamB(View view) {
+        scoreTeamB += 1;
+        displayForTeamB(scoreTeamB);
+    }
+
     public void resetScoreView(View view) {
         scoreTeamA = 0;
+        scoreTeamB = 0;
         displayForTeamA(scoreTeamA);
+        displayForTeamB(scoreTeamB);
     }
+
+
 }
+
+
+
