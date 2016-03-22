ff2a7f47a45d691248b80419865b371f5d807cd9
smalljay1117 <smalljay1117>
2016-03-22 12:13:59 +0800

    Fragment

---
+++ app/src/main/java/com/example/android/lab03_courtcounter/CourtCounterFragment.java
@@ -0,0 +1,96 @@
+package com.example.android.lab03_courtcounter;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class CourtCounterFragment extends Fragment implements View.OnClickListener {
+
+    private TextView m_tv_team_name;
+    private TextView m_tv_team_score;
+    private Button m_btn_three_points;
+    private Button m_btn_two_points;
+    private Button m_btn_free_throw;
+
+    public CourtCounterFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_court_counter, container, false);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        m_tv_team_name = (TextView)getView().findViewById(R.id.tv_team_name);
+        m_tv_team_score = (TextView)getView().findViewById(R.id.tv_team_score);
+
+        m_btn_three_points = (Button)getView().findViewById(R.id.btn_three_points);
+        m_btn_three_points.setOnClickListener(this);
+
+        m_btn_two_points = (Button)getView().findViewById(R.id.btn_two_points);
+        m_btn_two_points.setOnClickListener(this);
+
+        m_btn_free_throw = (Button)getView().findViewById(R.id.btn_free_throw);
+        m_btn_free_throw.setOnClickListener(this);
+    }
+
+    public void threeScoreView(View view) {
+        int three_score = Integer.parseInt(m_tv_team_score.getText().toString());
+        m_tv_team_score.setText(String.valueOf(three_score + 3));
+    }
+
+    public void twoScoreView(View view) {
+        int two_score = Integer.parseInt(m_tv_team_score.getText().toString());
+        m_tv_team_score.setText(String.valueOf(two_score + 2));
+    }
+
+    public void freeThrowView(View view) {
+        int free_throw = Integer.parseInt(m_tv_team_score.getText().toString());
+        m_tv_team_score.setText(String.valueOf(free_throw + 1));
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_three_points:
+//                int three_score = Integer.parseInt(m_tv_team_score.getText().toString());
+//                m_tv_team_score.setText(String.valueOf(three_score + 3));
+                threeScoreView(v);
+                break;
+            case R.id.btn_two_points:
+//                int two_score = Integer.parseInt(m_tv_team_score.getText().toString());
+//                m_tv_team_score.setText(String.valueOf(two_score + 2));
+                twoScoreView(v);
+                break;
+            case R.id.btn_free_throw:
+//                int free_throw = Integer.parseInt(m_tv_team_score.getText().toString());
+//                m_tv_team_score.setText(String.valueOf(free_throw + 1));
+                freeThrowView(v);
+                break;
+        }
+    }
+
+    public void resetscore() {
+        m_tv_team_score.setText("0");
+    }
+
+    public void setTeamName(CharSequence teamname) {
+        m_tv_team_name.setText(teamname);
+    }
+}
--- app/src/main/java/com/example/android/lab03_courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/lab03_courtcounter/MainActivity.java
@@ -1,13 +1,14 @@
 package com.example.android.lab03_courtcounter;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private int scoreTeamA;
     private int scoreTeamB;
+    private CourtCounterFragment m_fragment_team_a;
+    private CourtCounterFragment m_fragment_team_b;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,54 +16,37 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    private void displayForTeamA(int scoreA) {
-        TextView scoreView = (TextView) findViewById(R.id.teamA_a_score);
-        scoreView.setText(String.valueOf(scoreA));
-    }
-
-    public void add3ForTeamA(View view) {
-        scoreTeamA += 3;
-        displayForTeamA(scoreTeamA);
-    }
-
-    public void add2ForTeamA(View view) {
-        scoreTeamA += 2;
-        displayForTeamA(scoreTeamA);
-    }
-
-    public void add1ForTeamA(View view) {
-        scoreTeamA += 1;
-        displayForTeamA(scoreTeamA);
-    }
-
-    private void displayForTeamB(int scoreB) {
-        TextView scoreView = (TextView) findViewById(R.id.teamB_a_score);
-        scoreView.setText(String.valueOf(scoreB));
-    }
-
-    public void add3ForTeamB(View view) {
-        scoreTeamB += 3;
-        displayForTeamB(scoreTeamB);
-    }
-
-    public void add2ForTeamB(View view) {
-        scoreTeamB += 2;
-        displayForTeamB(scoreTeamB);
-    }
-
-    public void add1ForTeamB(View view) {
-        scoreTeamB += 1;
-        displayForTeamB(scoreTeamB);
+    @Override
+    protected void onStart() {
+        super.onStart();
+        setFragmentTeamName();
     }
 
     public void resetScoreView(View view) {
-        scoreTeamA = 0;
-        scoreTeamB = 0;
-        displayForTeamA(scoreTeamA);
-        displayForTeamB(scoreTeamB);
+//        scoreTeamA = 0;
+//        scoreTeamB = 0;
+
+        m_fragment_team_a.resetscore();
+        m_fragment_team_b.resetscore();
     }
 
 
+//    public void addThreePoints(View view) {
+////        CourtCounterFragment fragment_team_a = (CourtCounterFragment)getSupportFragmentManager().findFragmentById(R.id.fragment_team_a);
+////        fragment_team_a.m_tv_team_score.setText("3");
+//        LinearLayout linearLayout = (LinearLayout)view.getParent();
+//        TextView tv_team_score = (TextView)linearLayout.findViewById(R.id.tv_team_score);
+//        int score = Integer.parseInt(tv_team_score.getText().toString());
+//        tv_team_score.setText(String.valueOf(score + 3));
+//    }
+
+    public void setFragmentTeamName() {
+        m_fragment_team_a = (CourtCounterFragment)getSupportFragmentManager().findFragmentById(R.id.fragment_team_a);
+        m_fragment_team_b = (CourtCounterFragment)getSupportFragmentManager().findFragmentById(R.id.fragment_team_b);
+
+        m_fragment_team_a.setTeamName("黃蜂");
+        m_fragment_team_b.setTeamName("火箭");
+    }
 }
 
 
